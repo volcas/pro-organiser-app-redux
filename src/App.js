82897fdabbdb07d0416af8a96d98cafc4d6898b1
Nikhil Nair <nnair25@gmail.com>
2020-05-18 13:42:17 +0530

    Learner Solution

--- src/App.js
+++ src/App.js
@@ -1,34 +1,28 @@
-import React from 'react';
-import { Route, Switch, withRouter, Redirect } from 'react-router-dom';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { faList, faTrashAlt } from '@fortawesome/free-solid-svg-icons';
-
-import './App.css';
-
-import Boards from './../src/pages/Boards/Boards';
-import CreateBoard from './../src/pages/CreateBoard/CreateBoard';
-import Layout from './../src/pages/Layout/Layout';
-import Board from '../src/pages/Board/Board';
-
-library.add(faList, faTrashAlt);
+import React, { lazy, Suspense } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./redux/store";
+const Header = lazy(() => import("./components/Header/Header"));
+const Home = lazy(() => import("./containers/Home/Home"));
+const CreateBoard = lazy(() => import("./containers/CreateBoard/CreateBoard"));
+const Board = lazy(() => import("./containers/board/Board"));
 
 function App() {
-  let routes = (
-    <Switch>
-      <Route path="/createboard" component={CreateBoard}></Route>
-      <Route path="/board/:boardId" component={Board}></Route>
-      <Route path="/" component={Boards}></Route>
-      <Redirect to="/" />
-    </Switch>
-  );
-
   return (
-    <div className="App">
-      <Layout>
-        {routes}
-      </Layout>
-    </div>
+    <Provider store={store}>
+      <Router>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Header />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/createboard" component={CreateBoard} />
+            <Route path="/board/:name" component={Board} />
+            <Route exact path="*" component={Home} />
+          </Switch>
+        </Suspense>
+      </Router>
+    </Provider>
   );
 }
 
-export default withRouter(App);
+export default App;
--- src/common/Modal/Modal.js
+++ src/common/Modal/Modal.js
@@ -1,20 +1,10 @@
-import React, { Component } from 'react';
-import styles from './Modal.css';
+import React from 'react';
+import styles from './Modal.module.css';
 
-import Backdrop from './../Backdrop/Backdrop';
-
-class Modal extends Component {
-    render() {
-        return (
-            <div>
-                <Backdrop />
-                <span className={styles.Close} onClick={this.props.close}>X</span>
-                <div className={styles.Modal}>
-                    {this.props.content}
-                </div>
-            </div>
-        )
-    }
-}
-
-export default Modal;
+export const Modal = ({ children }) => {
+  return (
+    <div className={styles.modalContainer}>
+      <div className={styles.modal}>{children}</div>
+    </div>
+  );
+};
